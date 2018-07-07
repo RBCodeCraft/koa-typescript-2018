58bb0f4971ad524417087ba16c11d2cb5e0a445a
Russell Briggs <russ@paraflyer.net>
2018-07-07 12:40:02 +1200

    Re-order server code for easier copy-pasting

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,27 +1,29 @@
 
 import * as Koa from 'koa';
-
 import { config } from './config';
-import { registerRoutes } from './routes';
-import { registerServices } from './services';
 
 const app = new Koa();
 
 // Log Requests
 app.use(async (ctx, next) => {
     const start = new Date().getMilliseconds();
+
     await next();
+
     const responseTime = new Date().getMilliseconds() - start;
     console.log(`${ctx.status} ${ctx.method} ${ctx.url} - ${responseTime}ms`);
 });
 
-// Return Hello World!
-app.use(async (ctx, next) => {
-    ctx.body = 'Hello World!';
-});
-
+// Use routes and services
+import { registerRoutes } from './routes';
+import { registerServices } from './services';
 registerServices(app);
 registerRoutes(app);
+
+// Return Hello World!
+app.use(async (ctx) => {
+    ctx.body = 'Hello World!';
+});
 
 app.listen(config.port);
 
